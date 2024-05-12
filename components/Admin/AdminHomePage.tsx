a586efa516cea32b31099b94d41c45e6f33d96b9
mohamedelnaggar1223 <maelnaggar1223@gmail.com>
2024-05-12 22:20:09 +0300

    Charts

--- components/Admin/AdminHomePage.tsx
+++ components/Admin/AdminHomePage.tsx
@@ -2,6 +2,7 @@
 
 import { Card, LineChart, EventProps } from '@tremor/react';
 import { useState } from 'react';
+import { BarChart } from '@mui/x-charts/BarChart';
 
 const chartdata = [
   {
@@ -71,46 +72,47 @@ export default function AdminHomePage()
     const [value, setValue] = useState<EventProps>(null);
 
     return (
-        <section className='flex w-full flex-col overflow-auto gap-8 px-8 py-8'>
+        <section className='flex w-full flex-col overflow-auto gap-8 px-8 py-8 text-[#003B33]'>
             <div className='flex flex-col bg-[#fff] px-12 rounded-3xl border-[rgba(0,59,51,0.5)] gap-12 py-8 mx-auto border-8 w-screen max-w-[1080px]'>
 
                 <div className='flex gap-6'>
-                    <Card className="mx-auto max-w-md rounded-xl">
+                    <div className="mx-auto max-w-md rounded-xl shadow-lg flex-1 py-4 px-4">
                         <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
                             Total Donations
                         </h4>
                         <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
                             1,024
                         </p>
-                    </Card>
-                    <Card className="mx-auto max-w-md rounded-xl">
+                    </div>
+                    <div className="mx-auto max-w-md rounded-xl shadow-lg flex-1 py-4 px-4">
                         <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
                             Active Donors
                         </h4>
                         <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
                             72
                         </p>
-                    </Card>
-                    <Card className="mx-auto max-w-md rounded-xl">
+                    </div>
+                    <div className="mx-auto max-w-md rounded-xl shadow-lg flex-1 py-4 px-4">
                         <h4 className="text-tremor-default text-tremor-content dark:text-dark-tremor-content">
                             Total Organizations
                         </h4>
                         <p className="text-tremor-metric font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
                             16
                         </p>
-                    </Card>
+                    </div>
                 </div>
                 <div className='flex flex-col my-12 mx-auto gap-4 w-full'>
-                    <h3 className="text-lg font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">Active Users</h3>
-                    <LineChart
-                        className="h-[580px] max-w-6xl bg-white rounded-lg"
-                        data={chartdata}
-                        index="date"
-                        categories={['2022', '2023']}
-                        colors={['blue', 'red']}
-                        yAxisWidth={30}
-                        onValueChange={(v) => setValue(v)}
-                        connectNulls={true}
+                    <h3 className="text-lg font-medium text-tremor-content-strong dark:text-dark-tremor-content-strong">Active Donors</h3>
+                    <BarChart
+                        series={[
+                            { data: [35, 44, 24, 34] },
+                            { data: [51, 6, 49, 30] },
+                            { data: [15, 25, 30, 50] },
+                            { data: [60, 50, 15, 25] },
+                        ]}
+                        height={290}
+                        xAxis={[{ data: ['Q1', 'Q2', 'Q3', 'Q4'], scaleType: 'band' }]}
+                        margin={{ top: 10, bottom: 30, left: 40, right: 10 }}
                     />
                 </div>
             </div>
